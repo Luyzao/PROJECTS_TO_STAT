7a935fb3d5972e8f729d2d241810699421356047
Luiz Gustavo <jhhhhhhh58@gmail.com>
2023-01-23 20:43:13 -0300

    siderbar

---
+++ SIDEBAR/script.js
@@ -0,0 +1,10 @@
+function openMenu(){
+   const menuMobile = document.getElementById("menu-mobile");
+
+   if(menuMobile.className === "menu-mobile-active"){
+    menuMobile.className = "menu-mobile"
+   }
+    else{
+    menuMobile.className = "menu-mobile-active"
+    }
+}
